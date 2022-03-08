5ff309f9781fc52a8a43404559c44afe34239a70
Hifumisama <m.khayat92@gmail.com>
2022-03-08 14:40:53 +0100

    feat(addImage): add background and preload images

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 6501;
 
 // sendFile will go here
+app.use(express.static(path.join(__dirname, '/assets')));
 app.get('/', function (req, res)
 {
     res.sendFile(path.join(__dirname, '/index.html'));

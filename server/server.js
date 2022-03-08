6e61afc6762fc455dd7e9a0a1dd8417ddac06310
Hifumisama <m.khayat92@gmail.com>
2022-03-08 10:05:15 +0100

    feat(server): add server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function (req, res)
+{
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
